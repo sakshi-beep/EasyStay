fb581bdaa2663ba6caf48aa111f7969c0554a537
Arnold Joshua <119998819+Cybertech20@users.noreply.github.com>
2024-05-30 15:02:59 +0300

    Update pages-register.php

--- pages-register.php
+++ pages-register.php
@@ -1,5 +1,4 @@
 <?php
-
 ob_start();
 ?>
 
@@ -17,10 +16,8 @@ ob_start();
   <link href="assets/img/fav.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
 
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -29,7 +26,6 @@ ob_start();
   <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
 
   <link href="assets/css/style.css" rel="stylesheet">
 </head>
@@ -71,20 +67,30 @@ ob_start();
                       <label for="yourUsername" class="form-label">Admission Number</label>
                       <div class="input-group has-validation">
                         <input type="text" name="adm" class="form-control" id="yourUsername" required>
-                        <div class="invalid-feedback">Please enter your Id number.</div>
+                        <div class="invalid-feedback">Please enter your Admission number.</div>
                       </div>
                     </div>
 
                     <div class="col-12">
                       <label for="yourDep" class="form-label">Department</label>
-                      <input type="text" name="department" class="form-control" id="yourDep" required>
-                      <div class="invalid-feedback">Please, enter your department</div>
+                      <select name="department" class="form-control" id="yourDep" required onchange="populateCourses(this.value)">
+                        <option value="">Select Department</option>
+                        <option value="Information Technology">Information Technology</option>
+                        <option value="TV/Radio Program Production">TV/Radio Program Production</option>
+                        <option value="Film/Video Program Production">Film/Video Program Production</option>
+                        <option value="Engineering">Engineering Training</option>
+
+                      </select>
+                      <div class="invalid-feedback">Please, select your department</div>
                     </div>
 
                     <div class="col-12">
                       <label for="yourCourse" class="form-label">Course</label>
-                      <input type="text" name="course" class="form-control" id="yourCourse" required>
-                      <div class="invalid-feedback">Please, enter your course!</div>
+                      <select name="course" class="form-control" id="yourCourse" required>
+                        <option value="">Select Course</option>
+                        <!-- Options will be dynamically populated based on department selection -->
+                      </select>
+                      <div class="invalid-feedback">Please, select your course!</div>
                     </div>
 
                     <div class="col-12">
@@ -118,19 +124,30 @@ ob_start();
                       //echo "$name,$adm,$dep,$course,$email,$pass";
 
                       $connect = mysqli_connect("localhost","root","","kimc");
-                      $q = "INSERT INTO `stud` (`id`, `Name`, `ADM No`, `Department`, `Course`, `Email`, `Password`) VALUES (NULL, '$name', '$adm', '$dep', '$course', '$email', '$pass')";
-                      $e = mysqli_query($connect,$q);
+                      // Check if the email is already taken
+                          $check_query = "SELECT * FROM `stud` WHERE `Email` = '$email'";
+                          $check_result = mysqli_query($connect, $check_query);
 
-                      header("Location:index.php");
-                    }
-                  ?>
+                          if(mysqli_num_rows($check_result) > 0) {
+                              // Email already exists, show error message
+                              echo '<script>alert("Email already taken. Please choose a different email address.");</script>';
+                          } else {
+                              // Email is not taken, proceed with registration
+                              $q = "INSERT INTO `stud` (`id`, `Name`, `ADM No`, `Department`, `Course`, `Email`, `Password`) VALUES (NULL, '$name', '$adm', '$dep', '$course', '$email', '$pass')";
+                              $e = mysqli_query($connect,$q);
+
+                              // Redirect to login page after successful registration
+                              header("Location:index.php");
+                          }
+                      }
+                      ?>
                   </form>
 
                 </div>
               </div>
 
               <div class="credits">
-                Designed by <a href="mailto:arnoldjoshua78@gmail.com">Arnold Joshua</a>
+                Developed by <a href="mailto:arnoldjoshua78@gmail.com">Arnold Joshua</a>
               </div>
 
             </div>
@@ -144,7 +161,6 @@ ob_start();
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
   <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/chart.js/chart.umd.js"></script>
@@ -153,9 +169,34 @@ ob_start();
   <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
+
+  <script>
+    function populateCourses(department) {
+      var courseSelect = document.getElementById("yourCourse");
+      courseSelect.innerHTML = "";
+
+      if (department === "Information Technology") {
+        var options = ["Select Course", "Broadcast Journalism", "Advertising and Public Relations", "Journalism"];
+      } else if (department === "TV/Radio Program Production") {
+        var options = ["Select Course", "TV Production", " Radio Production"];
+      } else if (department === "Film/Video Program Production") {
+        var options = ["Select Course", "Production & Directing option", "Editing option", "Camera option", "Sound option", "Animation & Graphic Design"];
+      } else if (department === "Engineering") {
+        var options = ["Select Course", "Graphics and Web Design", "Instrument Option ", "Telecommunication Option", "Media Technology"];
+      }
+      else {
+        var options = ["Select Course"];
+      }
+
+      options.forEach(function(option) {
+        var opt = document.createElement("option");
+        opt.value = option;
+        opt.textContent = option;
+        courseSelect.appendChild(opt);
+      });
+    }
+  </script>
 
 </body>
 
